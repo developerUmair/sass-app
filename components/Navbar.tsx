eadcfc75f1ede3fde346396cb8fa41c7820d067c
developerUmair <umairmak143@gmail.com>
2025-07-01 23:19:22 +0500

    implemented clerk authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import NavItems from "./NavItems";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Navbar() {
   return (
@@ -12,7 +13,14 @@ export default function Navbar() {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+            <SignInButton>
+              <button className="btn-signin">Sign In</button>
+            </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
